5b79e7712599f99c6d03e617cee2270a14d46c81
Rinat Sagitov <sagitov256@gmail.com>
2026-09-04 20:34:07 +0500

    chore: initial commit

---
+++ todo_app/todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_appApp.swift
+//  todo_app
+//
+//  Created by Ринат Сагитов on 04.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ todo_appTests/todo_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  todo_appTests.swift
+//  todo_appTests
+//
+//  Created by Ринат Сагитов on 04.09.2026.
+//
+
+import Testing
+@testable import todo_app
+
+struct todo_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
